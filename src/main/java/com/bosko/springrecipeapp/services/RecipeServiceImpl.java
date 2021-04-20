258f36d648ab0f0d793f8db1704c5be60912ca63
boskodjokic <boskodjokic@yahoo.com>
2021-04-20 19:40:26 +0200

    Added project Lombok to project, added bootstrap to index page.

--- src/main/java/com/bosko/springrecipeapp/services/RecipeServiceImpl.java
+++ src/main/java/com/bosko/springrecipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.bosko.springrecipeapp.services;
 
 import com.bosko.springrecipeapp.domain.Recipe;
 import com.bosko.springrecipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,7 +21,7 @@ public class RecipeServiceImpl implements RecipeService {
     @Override
     public Set<Recipe> getRecipes() {
         Set<Recipe> recipeSet = new HashSet<>();
-
+        log.debug("In the Service");
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
 
         return recipeSet;
