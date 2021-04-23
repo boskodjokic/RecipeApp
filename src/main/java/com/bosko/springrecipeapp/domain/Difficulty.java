0a5b4a3979de724cc8f04ccdab15ca9495f0c624
boskodjokic <boskodjokic@yahoo.com>
2021-04-23 12:23:01 +0200

    Refactored dropdown for difficulty.

--- src/main/java/com/bosko/springrecipeapp/domain/Difficulty.java
+++ src/main/java/com/bosko/springrecipeapp/domain/Difficulty.java
@@ -1,6 +1,18 @@
 package com.bosko.springrecipeapp.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum Difficulty {
 
-    EASY, MODERATE, HARD
+    EASY("Easy"),
+    MODERATE("Moderate"),
+    HARD("Hard");
+
+    public final String value;
+
+    Difficulty(String value) {
+        this.value = value;
+    }
+
 }
