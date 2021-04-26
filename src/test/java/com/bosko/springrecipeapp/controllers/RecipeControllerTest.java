de8e346366fc79e4bc9c71698b043c7d1dbf9f99
boskodjokic <boskodjokic@yahoo.com>
2021-04-26 12:27:57 +0200

    Added validation to recipe command.

--- src/test/java/com/bosko/springrecipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/bosko/springrecipeapp/controllers/RecipeControllerTest.java
@@ -52,7 +52,7 @@ class RecipeControllerTest {
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("recipe/show"))
-        .andExpect(model().attributeExists("recipe"));
+                .andExpect(model().attributeExists("recipe"));
     }
 
     @Test
@@ -91,9 +91,28 @@ class RecipeControllerTest {
         when(recipeService.saveRecipeCommand(any())).thenReturn(command);
 
         mockMvc.perform(post("/recipe")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("description", "some String")
+                .param("directions", "some directions"))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+        )
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(view().name("recipe/recipeform"));
     }
 
     @Test
