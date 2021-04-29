72efbb800b38f19ac970d159a13b538fc2efb283
boskodjokic <boskodjokic@yahoo.com>
2021-04-29 10:23:35 +0200

    Adding dev and prod profiles.

--- src/main/java/com/bosko/springrecipeapp/controllers/IngredientController.java
+++ src/main/java/com/bosko/springrecipeapp/controllers/IngredientController.java
@@ -30,6 +30,7 @@ public class IngredientController {
 
     @GetMapping("/recipe/{recipeId}/ingredients")
     public String listIngredients (@PathVariable String recipeId, Model model) {
+
         log.debug("Getting ingredient list for recipe id: " + recipeId);
 
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
@@ -40,6 +41,7 @@ public class IngredientController {
     @GetMapping("recipe/{recipeId}/ingredient/{id}/show")
     public String showRecipeIngredient(@PathVariable String recipeId,
                                        @PathVariable String id, Model model) {
+
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
         return "recipe/ingredient/show";
     }
